564827dbc17dfa472989cc9320d45e7a3a366c80
KelDakroury <k.eldakroury@innopolis.university>
2021-04-09 20:47:35 +0300

    Ramadam updates

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
 """
-Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
-Deployed using heroku.
-Author: liuhh02 https://medium.com/@liuhh02
+reference:
+ https://medium.com/@liuhh02
 """
 import os
 import logging
 import gspread
 import sys
 
-
 from oauth2client.service_account import ServiceAccountCredentials
-from telegram.ext import Updater, CommandHandler
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from dotenv import load_dotenv
 from datetime import datetime, time, date, timedelta, timezone
 from calendar import monthrange
@@ -18,90 +17,139 @@ from dbhelper import DBHelper
 
 
 load_dotenv()
-# parsing data first!!, into a 2D array
-scope = [
+scopes = [
     "https://spreadsheets.google.com/feeds",
-    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scopes)
 gc = gspread.authorize(creds)
 worksheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1ozuBggiT-rBfnzP3zr6ubLOE2TtI1gY6nNtBUWbYLcE/edit#gid=1478484939')
 
 prayer_names = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
 
-#preparing for the database to store the  userids
+# Preparing for the database to store the  userids
 db = DBHelper()
-
-#set the port number to listen in for the webhook.
-PORT = int(os.environ.get('PORT', 5000))
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 TOKEN = os.getenv('BOT_TOKEN')
+
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 j = updater.job_queue
+
 moscow = timezone(timedelta(hours=3))
 
 def get_month_times():
+    '''Fetches today's prayer times and returns them as a list of 5 elements.'''
     now = date.today()
     sheet = worksheet.worksheet(date.strftime(now, '%B %Y'))
     days = monthrange(now.year, now.month)[1]
     columns = ['C', 'F', 'H', 'J', 'K']
-    ranges = [f'{c}4:{ c}{4+days-1}' for c in columns]
-    fajr, dohr, asr, maghrib, isha = [
+    ranges = [f'{c}4:{c}{4+days-1}' for c in columns]
+    prayers = [
         [cell[0] for cell in row]
         for row in sheet.batch_get(ranges)
     ]
-    return fajr, dohr, asr, maghrib, isha
+    return prayers
 
 
-def remind_next_prayer(context):
+def remind_next_prayer(context: CallbackContext):
+    '''Sends a message reminding about the prayer.'''
     prayer_name = context.job.context['prayer_name']
-    chat_id = context.job.context['uid']
+    chat_id = context.job.context['chat_id']
     context.bot.send_message(chat_id=chat_id,
-                             text=f"{prayer_name} prayer is NOW!")
+                             text=f"It's time for {prayer_name}!")
 
 
-def register_todays_prayers(context):
-    logging.info('Registering today\'s prayers')
+def register_todays_prayers(context: CallbackContext):
+    '''Registers callbacks for all of today's prayers.'''
+    uid = context.job.context['chat_id']
+    logging.info(f'Registering today\'s prayers for {uid}')
     prayer_times = get_month_times()
-    """for job in j.jobs:
-        job.schedule_removal()"""
-    for prayer in range(5):
-        prayer_time = prayer_times[prayer][datetime.now().day - 1]
+    today = datetime.now().day - 1
+    for name, prayer_time in zip(prayer_names, prayer_times):
+        prayer_time = prayer_time[today]
         timestamp = [int(x) for x in prayer_time.split(':')]
         timestamp = time(*timestamp, tzinfo=moscow)
-        logging.info(f'Registered callback for {prayer_names[prayer]} registered at {timestamp}')
+        # Don't register past prayers
+        if timestamp < datetime.now(moscow).time().replace(tzinfo=moscow):
+            continue
         j.run_once(remind_next_prayer, timestamp, context={
-            'uid': context.job.context['uid'], 
-            'prayer_name': prayer_names[prayer],
+            'chat_id': uid,
+            'prayer_name': name,
         })
+        logging.info(f'Registered callback for {name} for {uid} registered at {timestamp}')
 
-def start(update, context):
+def send_todays_times(update: Update, context: CallbackContext):
+    times = get_month_times()
+    today = datetime.now().day - 1
+    prayers = [f"*{name}*: {time[today]}" for name, time in zip(prayer_names, times)]
+    prayers_list = '\n'.join(prayers)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=f"Today's prayer times:\n{prayers_list}",
+                             parse_mode=ParseMode.MARKDOWN_V2)
+
+def start(update: Update, context: CallbackContext):
     new_id = update.effective_chat.id
-    db.add_id(new_id)
-    context.bot.send_message(chat_id=new_id,
-                             text="Prayer Times bot start")
+    context.chat_data['id'] = new_id
+    user = db.get_user(new_id)
+    # if user is not None and user.active:
+    #     context.bot.send_message(chat_id=new_id,
+    #                              text="The bot is already activated.""")
+    #     return
+
+    db.add_user(new_id)
+
     # now = datetime.now()
-    j.run_once(register_todays_prayers, 5, context={'uid': new_id})
-    j.run_daily(register_todays_prayers, time(hour=0), context={'uid': new_id})
+    job = j.run_daily(register_todays_prayers, time(hour=0), context={
+        'chat_id': new_id,
+    })
+    job.run(dispatcher) # Run just once (for today)
+    context.bot.send_message(chat_id=new_id,
+                             text="I will send you a reminder everyday on the prayer times of that day.\n"
+                                "Send /stop to stop reminding, /today to get just today's prayer times, "
+                                "and /start to start again.""")
+
+def stop(update: Update, context: CallbackContext):
+    uid = update.effective_chat.id
+    user = db.get_user(uid)
+
+    for job in j.jobs():
+        print(job)
+
+    context.bot.send_message(chat_id=uid,
+                             text="Sorry. Not yet implemented :(""",
+                             parse_mode=ParseMode.MARKDOWN_V2)
 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+today_handler = CommandHandler('today', send_todays_times)
+dispatcher.add_handler(today_handler)
+
+stop_handler = CommandHandler('stop', stop)
+dispatcher.add_handler(stop_handler)
+
+users = db.list_users()
+for user in users:
+    job = j.run_daily(register_todays_prayers, time(hour=0), context={
+        'chat_id': user.id,
+    })
+    job.run(dispatcher) # Run just once (for today)
+
 if 'ON_HEROKU' in os.environ:
+    # set the port number to listen in for the webhook.
+    port = int(os.environ.get('PORT', 5000))
     updater.start_webhook(listen="0.0.0.0",
-                            port=PORT,
+                            port=port,
                             url_path=TOKEN)
     updater.bot.setWebhook('https://stark-stream-60602.herokuapp.com/' + TOKEN)
 else:
     updater.start_polling()
 
 updater.idle()
-
-
--- dbhelper.py
+++ dbhelper.py
@@ -1,5 +1,12 @@
 import os
 import psycopg2
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    id: str
+    active: bool
 
 
 class DBHelper:
@@ -11,30 +18,69 @@ class DBHelper:
 
     def setup(self):
         cur = self.conn.cursor()
-        stmt = "CREATE TABLE IF NOT EXISTS userids (userid text)"
+        stmt = """
+            CREATE TABLE IF NOT EXISTS Users (
+                id INTEGER PRIMARY KEY,
+                active BOOLEAN NOT NULL
+            );
+        """
         cur.execute(stmt)
         self.conn.commit()
         cur.close()
 
-    def add_id(self, userid):
+    def add_user(self, user_id: str):
         cur = self.conn.cursor()
-        stmt = "INSERT INTO userids (userid) VALUES (%s);"
-        args = (userid, )
+        stmt = """
+            INSERT INTO Users (id, active)
+                VALUES (%s, TRUE)
+                ON CONFLICT (id) DO
+                    UPDATE SET active = TRUE;
+        """
+        args = (user_id, )
         cur.execute(stmt, args)
         self.conn.commit()
         cur.close()
 
-    def delete_id(self, userid):
+    def get_user(self, user_id: str) -> User:
         cur = self.conn.cursor()
-        stmt = "DELETE FROM userids WHERE userid = (%s);"
-        args = (userid, )
+        stmt = """
+            SELECT id, active FROM Users
+            WHERE id = %s;
+        """
+        args = (user_id,)
+        cur.execute(stmt, args)
+        user = cur.fetchone()
+        cur.close()
+        return User(user[0], user[1])
+
+    def set_active(self, user_id: str, active: bool):
+        cur = self.conn.cursor()
+        stmt = """
+            UPDATE Users
+            SET active = %s
+            WHERE id = %s;
+        """
+        args = (active, user_id)
+        cur.execute(stmt, args)
+        user = cur.fetchone()
+        cur.close()
+
+    def delete_user(self, user_id: str):
+        cur = self.conn.cursor()
+        stmt = """
+            DELETE FROM Users WHERE id = %s;
+        """
+        args = (user_id, )
         cur.execute(stmt, args)
         self.conn.commit()
         cur.close()
 
-    def get_items(self):
+    def list_users(self) -> List[User]:
         cur = self.conn.cursor()
-        stmt = "SELECT userid FROM userids;"
-        ids = [x[0] for x in cur.execute(stmt)]
+        stmt = """
+            SELECT id, active FROM Users;
+        """
+        cur.execute(stmt)
+        users = [User(x[0], x[1]) for x in cur.fetchall()]
         cur.close()
-        return ids
+        return users
