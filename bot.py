0aa077a558daad3e51ab38b013397886e5869862
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-04-02 16:26:21 +0300

    Add protection for the last day of the month

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ reference:
 import os
 import logging
 from datetime import datetime, time, timedelta, timezone
+from calendar import monthrange
 
 from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackContext
@@ -99,7 +100,15 @@ def send_todays_times(update: Update, context: CallbackContext):
 
 def send_tomorrows_times(update: Update, context: CallbackContext):
     times = get_month_times()
-    tomorrow = datetime.now(moscow).day
+    now = datetime.now(moscow)
+    _, days_in_month = monthrange(now.year, now.month)
+    tomorrow = now.day
+    if tomorrow >= days_in_month:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                text="Sorry, this feature doesn't work"
+                                     " on the last day of the month yet :(",
+                                parse_mode=ParseMode.MARKDOWN_V2)
+        return
     prayers = [f"*{name}*: {time[tomorrow]}" for name, time in zip(prayer_names, times)]
     prayers_list = '\n'.join(prayers)
     context.bot.send_message(chat_id=update.effective_chat.id,
