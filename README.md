131e4d1d2ed8b5d0b84ddfaf8bd526f9c681b42f
sh3b0 <ahmedshaban784@gmail.com>
2022-09-06 22:06:25 +0300

    Refactor project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# inno_prayer_bot
+# Telegram_Prayer_Bot
+
+## Data directory
+
+Files that should be in `/data` directory
+
+- `YYYY/MM.xlsx`
+    - First row should contain prayer names
+    - Rest of the rows should contain prayer times for the month
+- `YYYY/MM.pdf`: PDF schedules for convenience.
+
+## Development
+
+- The following environment variables should be set
+
+```text
+# .env.sample
+API_ID=
+API_HASH=
+API_TOKEN=
+DATABASE_URL=
+ADMIN_UID=
+```
+
+```bash
+git clone https://github.com/KelDakroury/Telegram_Prayer_Bot
+python -m venv venv
+sourve venv/bin/activate
+mv .env.sample .env
+python main.py
+```
+
+## Production
+
+```bash
+heroku login
+heroku git:remote -a $APP_NAME
+git push heroku main
+heroku ps:scale worker=1 -a $APP_NAME
+```
--- dbhelper.py
+++ dbhelper.py
@@ -1,16 +1,9 @@
 import os
 import psycopg2
-from typing import List
-from dataclasses import dataclass
-
-@dataclass
-class User:
-    id: str
-    active: bool
 
 
 class DBHelper:
-    def __init__(self, db_url: str=None):
+    def __init__(self, db_url: str = None):
         if db_url is None:
             db_url = os.environ['DATABASE_URL']
         self.conn = psycopg2.connect(db_url)
@@ -20,8 +13,7 @@ class DBHelper:
         cur = self.conn.cursor()
         stmt = """
             CREATE TABLE IF NOT EXISTS Users (
-                id INTEGER PRIMARY KEY,
-                active BOOLEAN NOT NULL
+                id INTEGER PRIMARY KEY
             );
         """
         cur.execute(stmt)
@@ -31,39 +23,13 @@ class DBHelper:
     def add_user(self, user_id: str):
         cur = self.conn.cursor()
         stmt = """
-            INSERT INTO Users (id, active)
-                VALUES (%s, TRUE)
-                ON CONFLICT (id) DO
-                    UPDATE SET active = TRUE;
-        """
-        args = (user_id, )
-        cur.execute(stmt, args)
-        self.conn.commit()
-        cur.close()
-
-    def get_user(self, user_id: str) -> User:
-        cur = self.conn.cursor()
-        stmt = """
-            SELECT id, active FROM Users
-            WHERE id = %s;
+            INSERT INTO Users (id)
+                VALUES (%s)
+                ON CONFLICT (id) DO NOTHING
         """
         args = (user_id,)
         cur.execute(stmt, args)
-        user = cur.fetchone()
-        if user is None:
-            return None
-        cur.close()
-        return User(user[0], user[1])
-
-    def set_active(self, user_id: str, active: bool):
-        cur = self.conn.cursor()
-        stmt = """
-            UPDATE Users
-            SET active = %s
-            WHERE id = %s;
-        """
-        args = (active, user_id)
-        cur.execute(stmt, args)
+        self.conn.commit()
         cur.close()
 
     def delete_user(self, user_id: str):
@@ -71,17 +37,17 @@ class DBHelper:
         stmt = """
             DELETE FROM Users WHERE id = %s;
         """
-        args = (user_id, )
+        args = (user_id,)
         cur.execute(stmt, args)
         self.conn.commit()
         cur.close()
 
-    def list_users(self) -> List[User]:
+    def list_users(self):
         cur = self.conn.cursor()
         stmt = """
-            SELECT id, active FROM Users;
+            SELECT id FROM Users;
         """
         cur.execute(stmt)
-        users = [User(x[0], x[1]) for x in cur.fetchall()]
+        users = list(map(lambda x: x[0], cur.fetchall()))
         cur.close()
         return users
